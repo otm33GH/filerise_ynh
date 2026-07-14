9cc037666844c33c1cfcbff7dc019c8d47b07860
otm33 <b.arrierg@orange.fr>
2026-07-14 18:56:47 +0200

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -20,10 +20,10 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
 // Define constants
 define('PERSISTENT_TOKENS_KEY', '__KEY__');
 define('PROJECT_ROOT', '__INSTALL_DIR__/');
-define('UPLOAD_DIR',    '__DATA_DIR__/uploads/');
-define('USERS_DIR',     '__DATA_DIR__/users/');
+define('UPLOAD_DIR',    '__INSTALL_DIR__/uploads/');
+define('USERS_DIR',     '__INSTALL_DIR__/users/');
 define('USERS_FILE',    'users.txt');
-define('META_DIR',      '__DATA_DIR__/metadata/');
+define('META_DIR',      '__INSTALL_DIR__/metadata/');
 define('META_FILE',     'file_metadata.json');
 define('TRASH_DIR',     UPLOAD_DIR . 'trash/');
 define('TIMEZONE',      '__TIMEZONE__');
